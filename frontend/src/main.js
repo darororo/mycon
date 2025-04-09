d2d09ea781ff46174ad8759da649e474adee0437
Vuth-Menghuor <hourousa9@gmail.com>
2025-04-09 17:04:40 +0700

    Install and configure PrimeVue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
+import "./assets/main.css";
+import { createApp } from "vue";
+import Aura from "@primevue/themes/aura";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
