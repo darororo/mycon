dc68cfd80d74fbc087975a4ff4d972d76582ee14
Vuth-Menghuor <hourousa9@gmail.com>
2025-04-24 12:04:08 +0700

    Add Primevue Card and Button components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,23 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import { Icon } from "@iconify/vue";
 import router from "./router";
+import Card from "primevue/card";
+import Button from "primevue/button";
 
 const app = createApp(App);
+
+// Register global components
 app.component("Icon", Icon);
+app.component("Card", Card);
+app.component("Button", Button);
+
+// use PrimeVue with Aura theme
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
   },
 });
+
+// Set up router and mount app
 app.use(router);
 app.mount("#app");
