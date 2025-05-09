1ba27ac650d77b20e391a4e1c40b37ef8138e8b7
darororo <daroyem99@gmail.com>
2025-05-09 11:05:52 +0700

    migrate Timeline component to nuxt

---
+++ frontend-nuxt/pages/project/timeline/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TimelineCard from '~/components/project/timeline/TimelineCard.vue';
+
+</script>
+
+<template>
+    <div>
+        <TimelineCard />
+    </div>
+</template>
