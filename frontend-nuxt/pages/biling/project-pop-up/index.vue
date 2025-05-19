77d4bb27c3afe5a94f940dc72ee46c70e54c0e0e
Vuth-Menghuor <hourousa9@gmail.com>
2025-05-19 12:33:43 +0700

    fix typo of billing directory

--- frontend-nuxt/pages/biling/project-pop-up/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import ProjectPopUp from '~/components/biling/project-pop-up/ProjectPopUp.vue'
-</script>
-
-<template>
-  <ProjectPopUp />
-</template>
---
+++ frontend-nuxt/pages/billing/project-pop-up/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ProjectPopUp from '~/components/billing/project-pop-up/ProjectPopUp.vue'
+</script>
+
+<template>
+  <ProjectPopUp />
+</template>
