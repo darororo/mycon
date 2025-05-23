ce4ba72813f40e2b6b25a594ce9325afdf96b76c
darororo <daroyem99@gmail.com>
2025-05-23 12:22:27 +0700

    add timeline page

---
+++ frontend-nuxt/pages/timeline/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-row justify-between h-full">
+    <div>
+      <PostCard />
+    </div>
+    <div class="sticky h-[10px] top-4">
+      <WorkflowSideWorkflow />
+    </div>
+  </div>
+</template>
