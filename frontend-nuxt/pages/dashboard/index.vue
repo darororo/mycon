73dd5965c791a9a24be7829dfc37ede96ac0c1ad
darororo <daroyem99@gmail.com>
2025-05-25 23:18:02 +0700

    update dashboard page

--- frontend-nuxt/pages/dashboard/index.vue
+++ frontend-nuxt/pages/dashboard/index.vue
@@ -2,10 +2,17 @@
 
 <template>
   <div>
-    <DashboardExpenseCard />
-    <div class="my-4">
+    <div class="flex flex-row justify-between items-center mb-2">
+      <h2 class="text-black text-2xl font-bold">Dashboard</h2>
       <DashboardNotificationCard />
     </div>
+    <div class="flex flex-row max-w-[1440px] gap-2 p-4 rounded-md border-2 border-gray-200">
+      <DashboardExpenseCard />
+      <div class="w-full max-w-[800px] h-auto">
+        <DashboardTotalExpenseChart />
+      </div>
+    </div>
+
     <div class="flex flex-row gap-2 max-w-[1440px]">
       <div class="w-full">
         <DashboardCategoryExpenseChart title="Labor Expense" />
