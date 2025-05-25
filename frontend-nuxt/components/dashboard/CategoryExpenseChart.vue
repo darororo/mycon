c0a9b73a8bbba5ffda9e320656de76a0af1194d3
darororo <daroyem99@gmail.com>
2025-05-25 22:19:48 +0700

    component/dashboard: add CategoryExpenseChart

---
+++ frontend-nuxt/components/dashboard/CategoryExpenseChart.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="p-4 rounded-md border-gray-400 border-2">
+    <div class="flex flex-row justify-between items-center pb-4">
+      <h2 class="text-black font-[Montserrat] font-weight-bold">{{ title }}</h2>
+      <Select
+        :dt="{
+          background: 'none',
+          color: 'black',
+          focus: {
+            borderColor: 'black',
+          },
+        }"
+        placeholder="Filter"
+      />
+    </div>
+    <Chart
+      type="bar"
+      :data="chartData"
+      :options="chartOptions"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Chart from 'primevue/chart'
+
+import { ref, onMounted } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+  },
+})
+
+onMounted(() => {
+  chartData.value = setChartData()
+  chartOptions.value = setChartOptions()
+})
+
+const chartData = ref()
+const chartOptions = ref()
+
+const setChartData = () => {
+  const sites = ['Site A', 'Site B', 'Site C']
+
+  return {
+    labels: sites,
+    backgroundColor: ['#F8BBD0', '#C5CAE9', '#FFF9C4'],
+
+    datasets: [
+      {
+        label: 'Inventory',
+        data: [540, 325, 702],
+        borderRadius: 14,
+        hoverBorderColor: ['#8FFF93'],
+        hoverBorderWidth: 2,
+      },
+      {
+        label: 'Labor',
+        data: [540, 325, 702],
+        borderRadius: 14,
+        hoverBorderColor: ['#8FFF93'],
+        hoverBorderWidth: 2,
+      },
+      {
+        label: 'Material',
+        data: [540, 325, 702],
+        borderRadius: 14,
+        hoverBorderColor: ['#8FFF93'],
+        hoverBorderWidth: 2,
+      },
+    ],
+  }
+}
+
+const setChartOptions = () => {
+  return {
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+          color: 'black',
+        },
+      },
+      title: {
+        display: true,
+        text: 'Site Expense',
+      },
+    },
+  }
+}
+</script>
--- frontend-nuxt/pages/dashboard/index.vue
+++ frontend-nuxt/pages/dashboard/index.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts"></script>
+
 <template>
   <div>
     <DashboardExpenseCard />
     <div class="my-4">
       <DashboardNotificationCard />
+    </div>
+    <div class="flex flex-row gap-2 max-w-[1440px]">
+      <div class="w-full">
+        <DashboardCategoryExpenseChart title="Labor Expense" />
+      </div>
+      <div class="w-full">
+        <DashboardCategoryExpenseChart title="Material Expense" />
+      </div>
     </div>
     <TopProjectItem />
   </div>
