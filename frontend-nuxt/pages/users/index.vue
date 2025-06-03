f2cebebfad8a2ba9f44fa728a7b616a494398e58
Vuth-Menghuor <hourousa9@gmail.com>
2025-06-03 10:54:43 +0700

    add title for page

--- frontend-nuxt/pages/users/index.vue
+++ frontend-nuxt/pages/users/index.vue
@@ -4,9 +4,21 @@ import UserPage from '~/components/user/userPage.vue'
 </script>
 
 <template>
-  <div style="display: flex; flex-direction: row; width: 100%; gap: 20px">
-    <ContextMenu />
-    <UserPage />
+  <div>
+    <h1
+      style="
+        font-family: 'Montserrat', sans-serif;
+        color: black;
+        margin: 8px 0 20px 0;
+        font-size: 20px;
+        font-weight: 600;
+      "
+    >
+      Account Setting
+    </h1>
+    <div style="display: flex; flex-direction: row; width: 100%; gap: 20px">
+      <ContextMenu />
+      <UserPage />
+    </div>
   </div>
 </template>
-llo
