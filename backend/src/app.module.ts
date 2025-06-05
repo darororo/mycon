7a6ae7b46e487a9c08531a19b68b901b03ac9af0
darororo <daroyem99@gmail.com>
2025-06-06 00:50:42 +0700

    install minio service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,18 +8,37 @@ import { ProjectsModule } from './projects/projects.module';
 import { ManagersModule } from './managers/managers.module';
 import { OwnersModule } from './owners/owners.module';
 import { WorkersModule } from './workers/workers.module';
+import { NestMinioModule } from 'nestjs-minio';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5435,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'mycon_dev',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USER'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: config.get<boolean>('DB_SYNCHRONIZE'),
+          // dropSchema: true,
+        };
+      },
+    }),
+    NestMinioModule.register({
+      endPoint: 'localhost',
+      port: 9000,
+      useSSL: false,
+      accessKey: process.env.MINIO_ACCESS_KEY,
+      secretKey: process.env.MINIO_SECRET_ACCESS_KEY,
     }),
     UsersModule,
     ProjectsModule,
