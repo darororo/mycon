5b3635140ba12167bbd596598bfc5a5b096bc0ad
darororo <daroyem99@gmail.com>
2025-06-11 00:43:09 +0700

    backend: resize uploaded image and store in minio

--- backend/src/upload/upload.service.ts
+++ backend/src/upload/upload.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Client } from 'minio';
 import { InjectMinio } from 'nestjs-minio';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class UploadService {
@@ -11,6 +12,15 @@ export class UploadService {
       throw new NotFoundException();
     }
 
+    const smallImg = this.resizeImageToSmall(file.buffer);
+    const smallImgPath = 'small/' + file.originalname;
+
+    await this.minioClient.putObject('mycon', smallImgPath, smallImg);
+
     return this.minioClient.putObject('mycon', file.originalname, file.buffer);
+  }
+
+  resizeImageToSmall(imgBuffer: Buffer) {
+    return sharp(imgBuffer).resize({ width: 300 });
   }
 }
