5d38092ae241e74df3540f13c190607c5844d8e0
Chhoungseang <chhoungseangung@gmail.com>
2025-06-15 13:37:51 +0700

    updated inventory module

--- backend/src/inventory/dto/create-inventory.dto.ts
+++ backend/src/inventory/dto/create-inventory.dto.ts
@@ -1,16 +1,20 @@
-import { IsEnum, IsNumber, IsString } from "class-validator";
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { QuantityUnit } from "../enum/unit.enum";
 
 export class CreateInventoryDto {
     @IsString()
+    @IsNotEmpty()
     itemname: string;
 
     @IsString()
+    @IsNotEmpty()
     category: string;
 
     @IsNumber()
+    @IsNotEmpty()
     quantity: number;
 
     @IsEnum(QuantityUnit)
+    @IsNotEmpty()
     unit: QuantityUnit;
 }
