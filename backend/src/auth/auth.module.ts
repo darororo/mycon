924469f591e1c79c39b845e0e6425f74636ed749
SorThanaroth <sorthanaroth018@gmail.com>
2025-06-23 22:39:26 +0700

    fix: google sign up and response user sign in

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auth } from './entities/auth.entity';
 import { UsersModule } from 'src/users/users.module';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auth]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Auth, User]), UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService]
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { AuthInput, AuthResult, SignInData } from './types/auth-data.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
     private userService: UsersService,
     private jwtService: JwtService,
   ) { }
@@ -38,7 +44,11 @@ export class AuthService {
     const accessToken = await this.jwtService.signAsync(tokenPayload);
     let authUser
     if (accessToken) {
-      authUser = await this.userService.findOne(user.userId)
+      authUser = await this.userRepository
+        .createQueryBuilder('user')
+        .leftJoinAndSelect('user.photos', 'photos')
+        .where('user.id = :id', { id: user.userId })
+        .getOne();
     }
 
     return {
